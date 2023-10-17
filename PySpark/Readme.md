d57d54dd9dde03e49032b7ea2f90c45218fef2ad
vishwa3908 <vishwaranjan969@gmail.com>
2023-10-17 11:09:10 +0530

    simple pyspark code

---
+++ PySpark/Readme.md
@@ -0,0 +1,5 @@
+ **Start Spark Session**
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('app').getOrCreate()
---
+++ PySpark/app.py
@@ -0,0 +1,10 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('app').getOrCreate()
+
+sc = spark.sparkContext
+print(sc)
+rdd = sc.parallelize([1,2,3,4])
+print(rdd.collect())
+df_1 = spark.read.csv("machine-readable-business-employment-data-jun-2023-quarter.csv",header=True)
+print(df_1.show())
