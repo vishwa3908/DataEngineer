517aae8e54d8b5adaa4c116cb85597322fa126cf
vishwa3908 <vishwaranjan969@gmail.com>
2024-02-21 11:03:12 +0530

    fixed git issue

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -3,7 +3,6 @@
 */
 
 /*markdown
-
 ###   Question 1
 
 Challenge : Find the origin and the destination of each customer.
@@ -20,7 +19,6 @@ flights_data = [(1,'Flight1' , 'Delhi' , 'Hyderabad'),
  ]
 
 _schema = "cust_id int, flight_id string , origin string , destination string"
-
 
 */
 
@@ -86,12 +84,9 @@ combined_temp as
 select * from combined_temp;
 
 
-
 /*markdown
-
 ## Question 2
 how can you add one or multiple column before or after a specific column in mysql
-
 
 
 */
@@ -101,7 +96,6 @@ how can you add one or multiple column before or after a specific column in mysq
 -- ADD COLUMN new_column1 datatype1 AFTER existing_column1,
 -- ADD COLUMN new_column2 datatype2 AFTER existing_column2,
 -- ...;
-
 
 
 
@@ -133,9 +127,8 @@ select name from employee where lower(name) regexp '[^aeiou]$';
 select name from employee where lower(name) regexp '[aeiou]$';
 
 /*markdown
+
 #### find names of employee whose name starts with vowel but not end with vowel
-
-
 
 */
 
@@ -170,6 +163,9 @@ select * from test_join_1;
 
 
 
+
+
+
 select * from test_join t 
 inner join test_join_1 t1 
 on t.id=t1.id;
@@ -185,10 +181,10 @@ right join test_join t1
 on t.id=t1.id;
 
 
-
 /*markdown
 #### 𝑪𝒉𝒂𝒍𝒍𝒆𝒏𝒈𝒆 : Get the name of the 𝐡𝐢𝐠𝐡𝐞𝐬𝐭 𝐬𝐚𝐥𝐚𝐫𝐲 𝐚𝐧𝐝 𝐥𝐨𝐰𝐞𝐬𝐭 𝐬𝐚𝐥𝐚𝐫𝐲 employee name in 𝐞𝐚𝐜𝐡 𝐝𝐞𝐩𝐚𝐫𝐭𝐦𝐞𝐧𝐭. If the salary is same then return the name of the employee whose name comes first in 𝐥𝐞𝐱𝐢𝐜𝐨𝐠𝐫𝐚𝐩𝐡𝐢𝐜𝐚𝐥 𝐨𝐫𝐝𝐞𝐫.
 ![Alt text](image-1.png)
+
 */
 
 -- first approach
@@ -232,3 +228,361 @@ FROM
 GROUP BY
     department;
 
+
+
+/*markdown
+
+## Employees Earning More Than Their Managers
+
+ https://thecleverprogrammer.com/2023/05/24/employees-earning-more-than-their-managers-using-sql/
+
+*/
+
+-- CREATE TABLE if not exists emp (
+--   id INT PRIMARY KEY,
+--   name VARCHAR(255),
+--   salary INT,
+--   managerId INT
+-- );
+
+-- INSERT INTO emp (id, name, salary, managerId) VALUES
+--   (1, 'Rahul', 5000, 3),
+--   (2, 'Rohit', 8000, 4),
+--   (3, 'Suresh', 6000, NULL),
+--   (4, 'Manish', 9000, 3);
+
+select * from emp;
+
+select a.id,a.name,a.salary,a.managerId
+from emp a
+join emp b
+on a.managerId=b.id
+where a.salary > b.salary;
+
+/*markdown
+## Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+*/
+
+drop TABLE if exists person;
+CREATE TABLE Person (
+    id INT PRIMARY KEY,
+    email VARCHAR(255)
+);
+
+INSERT INTO Person (id, email) VALUES
+    (1, 'john@example.com'),
+    (2, 'bob@example.com'),
+    (3, 'john@example.com'),
+    (4, 'alice@example.com'),
+    (5, 'charlie@example.com'),
+    (6, 'david@example.com'),
+    (7, 'alice@example.com'),
+    (8, 'bob@example.com'),
+    (9, 'emily@example.com'),
+    (10, 'john@example.com'),
+    (11, 'frank@example.com'),
+    (12, 'helen@example.com'),
+    (13, 'david@example.com'),
+    (14, 'bob@example.com'),
+    (15, 'emily@example.com');
+
+
+
+
+select * from person;
+
+-- DELETE p
+-- FROM Person p
+-- JOIN (
+--     SELECT id, email, ROW_NUMBER() OVER (PARTITION BY email ORDER BY email ASC) AS roww
+--     FROM Person
+-- ) temp ON p.id = temp.id
+-- WHERE temp.roww != 1;
+
+
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT id
+    FROM (
+        SELECT id, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS row_num
+        FROM Person
+    ) AS temp
+    WHERE row_num = 1
+);
+
+
+
+
+SELECT * from person;
+
+/*markdown
+## Concept of Recursive CTE
+*/
+
+/*markdown
+WITH RECURSIVE cte_name AS
+(
+    SELECT query (Non Recursive query  or the Base query)
+    UNION ALL
+    SELECT query (Recursive query  using cte_name [with a termination condition])
+)
+select * from cte_name;
+*/
+
+create database if not exists relations;
+use relations;
+show tables;
+CREATE TABLE if not exists employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(255) NOT NULL,
+    manager_id INT
+    -- Add other columns as needed
+);
+
+-- INSERT INTO employees (employee_id, employee_name, manager_id) VALUES
+--     (1, 'John Doe', NULL),        -- CEO, top-level manager
+--     (2, 'Alice Smith', 1),        -- Manager reporting to CEO
+--     (3, 'Bob Johnson', 2),        -- Employee reporting to Alice
+--     (4, 'Charlie Brown', 3),     -- Employee reporting to Bob
+--     (5, 'Diana Williams', 2),     -- Another employee reporting to Alice
+--     (6, 'Eva Davis', 5);          -- Employee reporting to Diana
+
+
+
+
+select * from relations.employees;
+
+/*markdown
+Question 1: find all heiarchy as column and also all manager nbame concatenated as column if no managet null
+*/
+
+use relations;
+select * from employees order by manager_id asc;
+
+
+WITH RECURSIVE EmployeeLevels AS (
+    SELECT
+        employee_id,
+        manager_id,
+        0 AS level
+    FROM
+        employees
+    WHERE
+        manager_id IS NULL -- Assuming the top-level manager has a NULL manager_id
+
+    UNION ALL
+
+    SELECT
+        e.employee_id,
+        e.manager_id,
+        el.level + 1
+    FROM
+        employees e
+    JOIN
+        EmployeeLevels el ON e.manager_id = el.employee_id
+)
+SELECT
+    employee_id,
+    manager_id,
+    level
+FROM
+    EmployeeLevels;
+
+
+
+
+WITH RECURSIVE EmployeeLevels AS (
+    SELECT
+        employee_id,
+        manager_id,
+        employee_name,
+        employee_name AS manager_names,
+        0 AS level
+    FROM
+        employees
+    WHERE
+        manager_id IS NULL
+
+    UNION ALL
+
+    SELECT
+        e.employee_id,
+        e.manager_id,
+        e.employee_name,
+        CONCAT(el.manager_names, ' > ', e.employee_name),
+        el.level + 1
+    FROM
+        employees e
+    JOIN
+        EmployeeLevels el ON e.manager_id = el.employee_id
+)
+SELECT
+    employee_id,
+    employee_name,
+    manager_names,
+    level
+FROM
+    EmployeeLevels;
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*markdown
+## Question
+
+Write a query that'll identify returning active users. A returning active user is a user that has made a second purchase within 7 days of any other of their purchases. Output a list of user_ids of these returning active users.
+![alt text](image-2.png)
+
+
+<!-- create table amazon_interview (
+user_id int,
+item varchar(50),
+created_at date,
+revenue bigint
+);
+
+insert into amazon_interview(user_id , item , created_at, revenue)
+values(100, 'bread', '07-03-2020', 410),(100, 'banana' ,'13-03-2020' ,175),
+(100, 'banana','29-03-2020', 599),
+(101, 'milk', '01-03-2020', 449),
+(101, 'milk', '26-03-2020', 740),
+(114, 'banana', '10-03-2020', 200),
+(114, 'biscuit', '16-03-2020', 300); -->
+*/
+
+-- create table amazon_interview (
+-- user_id int,
+-- item varchar(50),
+-- created_at date,
+-- revenue bigint
+-- );
+
+-- INSERT INTO amazon_interview (user_id, item, created_at, revenue)
+-- VALUES
+-- (100, 'bread', '2020-03-07', 410),
+-- (100, 'banana', '2020-03-13', 175),
+-- (100, 'banana', '2020-03-29', 599),
+-- (101, 'milk', '2020-03-01', 449),
+-- (101, 'milk', '2020-03-26', 740),
+-- (114, 'banana', '2020-03-10', 200),
+-- (114, 'biscuit', '2020-03-16', 300);
+
+
+
+-- select * from amazon_interview;
+
+-- with cte as
+-- (
+--     select * from(SELECT *,lag(created_at) over(partition by user_id order by created_at) as previous_day from amazon_interview)temp where previous_day is not null
+-- )select distinct(user_id) from cte where TIMESTAMPDIFF(DAY, previous_day, created_at) <=7;
+
+/*markdown
+#### Question :- Calculate the % Marks for each student. Each subject is of 100 marks. Create a result column by following the below condition 
+![alt text](image-3.png)
+*/
+
+
+select * from student;
+select * from marks;
+with cte as
+(
+    
+)
+
+/*markdown
+### Question 
+Get the name of the 𝐡𝐢𝐠𝐡𝐞𝐬𝐭 𝐬𝐚𝐥𝐚𝐫𝐲 𝐚𝐧𝐝 𝐥𝐨𝐰𝐞𝐬𝐭 𝐬𝐚𝐥𝐚𝐫𝐲 employee name in 𝐞𝐚𝐜𝐡 𝐝𝐞𝐩𝐚𝐫𝐭𝐦𝐞𝐧𝐭. If the salary is same then return the name of the employee whose name comes first in 𝐥𝐞𝐱𝐢𝐜𝐨𝐠𝐫𝐚𝐩𝐡𝐢𝐜𝐚𝐥 𝐨𝐫𝐝𝐞𝐫.
+*/
+
+select * from employee;
+with cte as
+(
+    select name,department,salary,
+    dense_rank() over(partition by department order by salary desc,name desc) as rank1,
+    dense_rank() over(partition by department order by salary ,name ) as rank2
+    from employee
+)
+select department,
+max(case when rank1 = 1 then name end)as max_salary,
+    max(case when rank2 = 1 then name end)as min_salary
+from cte
+group by department;
+
+
+
+/*markdown
+### Question 
+
+Retrieve information about consecutive login streaks for employee who have logged in for at least two consecutive days.
+
+For each employee provide the emp_id , the number of consecutive days logged in ,the start_date of the streak and end_date of the streak.
+
+![alt text](image-4.png)
+*/
+
+-- CREATE TABLE if not exists pwc_attandance_log (
+--  emp_id INT,
+--  log_date DATE,
+--  flag CHAR
+-- );
+-- insert into pwc_attandance_log(emp_id,log_date,flag) values
+-- (101, '2024-01-02', 'N'),
+-- (101, '2024-01-03', 'Y'),
+-- (101, '2024-01-04', 'N'),
+-- (101, '2024-01-07', 'Y'),
+-- (102, '2024-01-01', 'N'),
+-- (102, '2024-01-02', 'Y'),
+-- (102, '2024-01-03', 'Y'),
+-- (102, '2024-01-04', 'N'),
+-- (102, '2024-01-05', 'Y'),
+-- (102, '2024-01-06', 'Y'),
+-- (102, '2024-01-07', 'Y'),
+-- (103, '2024-01-01', 'N'),
+-- (103, '2024-01-04', 'N'),
+-- (103, '2024-01-05', 'Y'),
+-- (103, '2024-01-06', 'Y'),
+-- (103, '2024-01-07', 'N');
+
+-- select * from pwc_attandance_log;
+WITH cte AS (
+    SELECT *,
+           DATEDIFF(log_date, previous_login) AS diff
+    FROM (
+        SELECT *,
+               LAG(log_date) OVER(PARTITION BY emp_id ORDER BY log_date) AS previous_login
+        FROM pwc_attandance_log
+    ) AS temp
+    WHERE previous_login IS NOT NULL
+      AND flag <> 'N'
+      AND DATEDIFF(log_date, previous_login) = 1
+)
+SELECT *
+FROM cte;
+
+
+/*markdown
+### Question 6
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+![alt text](image-5.png)
+*/
+
+select * from class;
+select *,
+case when count(Student)%2=0 then 'odd'
+else 'even' end as 'check'
+from class
+group by Student;
+
+/*markdown
+
+*/
---
+++ SQL/query_6.sql
@@ -0,0 +1,37 @@
+/*markdown
+### Question
+Given a table of purchases by date, calculate the month-over-month 𝐩𝐞𝐫𝐜𝐞𝐧𝐭𝐚𝐠𝐞 𝐜𝐡𝐚𝐧𝐠𝐞 𝐢𝐧 𝐫𝐞𝐯𝐞𝐧𝐮𝐞. The output should include the year-month date (YYYY-MM) and percentage change, rounded to the 2nd decimal point, and sorted from the beginning of the year to the end of the year.
+![alt text](image-8.png)
+*/
+
+-- create table amazon_monthly_rev (
+-- id int,
+-- created_at date ,
+-- value int ,
+-- purchase_id int
+-- );
+
+-- insert into amazon_monthly_rev(id , created_at , value , purchase_id)
+-- values(1,'2019-01-01',172692,43),
+-- (2,'2019-01-05',177194,36),
+-- (3,'2019-02-06',116948,56),
+-- (4,'2019-02-10',162515,29),
+-- (5,'2019-02-14',120741,30),
+-- (6,'2019-03-22',151688,34),
+-- (7,'2019-03-26',1002327,44);
+
+select * from amazon_monthly_rev;
+
+with cte as(
+    SELECT 
+       CONCAT(YEAR(created_at), '-', LPAD(MONTH(created_at), 2, '0')) as yearmonth,
+       sum(value) as total,
+       lag(sum(value)) 
+       over(order by CONCAT(YEAR(created_at), '-', LPAD(MONTH(created_at), 2, '0')) ) as previous
+    FROM amazon_monthly_rev
+    group by 1 order by 1
+)
+select yearmonth as YearMonth,
+    round((abs(previous-total)/(previous))*100,2) as precentage_revenue_diff from cte;
+
+
